a90d5a647d67277b173efddbd26217fae7c7fa99
achieva.gemilang <achievafuturagemilang@gmail.com>
2026-01-05 21:09:55 +0700

    feat: solve 113, 46, 77, 78, 1926, 112, 15

---
+++ leetcode/backtracking/78-subsets/78-subsets.java
@@ -0,0 +1,23 @@
+class Solution {
+    public List<List<Integer>> subsets(int[] nums) {
+        List<List<Integer>> ans = new ArrayList<>();
+
+        List<Integer> subset = new ArrayList<>();
+        dfs(0, ans, subset, nums);
+        return ans;
+    }
+
+    void dfs(int start, List<List<Integer>> ans, List<Integer> subset, int[] nums){
+        if(start >= nums.length){
+            ans.add(new ArrayList<>(subset));
+            return;
+        }
+        // take
+        subset.add(nums[start]);
+        dfs(start+1, ans, subset, nums);
+
+        // not take
+        subset.remove(subset.size() - 1);
+        dfs(start+1, ans, subset, nums);
+    }
+}
