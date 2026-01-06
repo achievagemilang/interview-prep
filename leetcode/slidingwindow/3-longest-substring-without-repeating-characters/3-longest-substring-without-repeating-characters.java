9b4ce17f815b0745f0b4db993cd20655cdbcd860
achieva.gemilang <achievafuturagemilang@gmail.com>
2026-01-06 12:26:22 +0700

    feat: solve 1, 1161, 2, 3

---
+++ leetcode/slidingwindow/3-longest-substring-without-repeating-characters/3-longest-substring-without-repeating-characters.java
@@ -0,0 +1,26 @@
+class Solution {
+    public int lengthOfLongestSubstring(String s) {
+        if(s.length() == 0) return 0;
+        int l = 0;
+        Set<Character> inWindow = new HashSet<>();
+        int maxVal = -1;
+        for(int r=0; r<s.length(); r++){
+            while (inWindow.contains(s.charAt(r))) {
+                inWindow.remove(s.charAt(l));
+                l++;
+            }
+            inWindow.add(s.charAt(r));
+            maxVal = Math.max(maxVal, r - l + 1);
+        }
+
+        return maxVal;
+
+    }
+}
+
+
+/*
+Using sliding window to keep track of repeating character, delete if found
+T(n) -> O(n)
+S(n) -> O(n)
+*/
